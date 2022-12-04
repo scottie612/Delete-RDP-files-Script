0b9025c5d9f1cd2d8df3183cb78bc2ec862d428c
scottie612 <scottie61224@gmail.com>
2022-12-03 19:21:47 -0500

    Initial commit

---
+++ Downloads Cleaner/Downloads Cleaner/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Downloads_Cleaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\scott\Downloads";
+
+            Console.WriteLine("Starting....");
+            Console.WriteLine("Deleting Files in " +path);
+            
+            string[] directoryFiles = System.IO.Directory.GetFiles(path, "*.rdp");
+            foreach (string directoryFile in directoryFiles)
+            {
+                Console.WriteLine("...Deleting file : " + directoryFile);
+                System.IO.File.Delete(directoryFile);
+            }
+
+            Console.WriteLine("Finished....");
+            Console.ReadLine();
+        }
+    }
+}
