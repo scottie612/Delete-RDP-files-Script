889ea72ae6260e4d40b847761c58e98d87301f7c
scottie612 <scottie61224@gmail.com>
2022-12-12 08:49:21 -0500

    Update to make it find config.json

--- Downloads Cleaner/Downloads Cleaner/Program.cs
+++ Downloads Cleaner/Downloads Cleaner/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Reflection;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Downloads_Cleaner
 {
@@ -6,10 +10,19 @@ namespace Downloads_Cleaner
     {
         static void Main(string[] args)
         {
-            string path = @"C:\Users\scott\Downloads";
+            string myExeDir = new FileInfo(Assembly.GetEntryAssembly().Location).Directory.ToString();
+            var configPath = myExeDir + "\\config.json";
+            Console.WriteLine(configPath);
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(configPath, optional: false);
+
+            IConfiguration config = builder.Build();
+
+            string path = config.GetSection("PathToDeleteFrom").Value;
 
             Console.WriteLine("Starting....");
-            Console.WriteLine("Deleting Files in " +path);
+            Console.WriteLine("Deleting Files in " + path);
             
             string[] directoryFiles = System.IO.Directory.GetFiles(path, "*.rdp");
             foreach (string directoryFile in directoryFiles)
@@ -19,7 +32,7 @@ namespace Downloads_Cleaner
             }
 
             Console.WriteLine("Finished....");
-            Console.ReadLine();
+       
         }
     }
 }
